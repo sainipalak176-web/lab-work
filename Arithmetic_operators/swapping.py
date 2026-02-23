8d6f41992da24dd7baa65a5cee5ce57015e25b3e
sainipalak176-web <sainipalak176@gmail.com>
2026-02-23 18:13:59 +0530

    Add files via upload

---
+++ Arithmetic_operators/swapping.py
@@ -0,0 +1,17 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
+
+#output
+"""Enter first number: 23
+Enter second number: 54
+After Swapping:
+a = 54
+b = 23"""

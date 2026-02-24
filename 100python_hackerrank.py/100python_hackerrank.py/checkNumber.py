3c9759501fd84653c3943dbc4970a4edfeabd0ed
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 11:29:42 +0530

    Add files via upload

---
+++ 100python_hackerrank.py/100python_hackerrank.py/checkNumber.py
@@ -0,0 +1,20 @@
+#check number is positive ,negative or zero
+n = int(input("Enter the number: "))
+
+
+if(n>0):
+    print("number is positive")
+elif(n<0):
+    print("number is negative")
+else:
+    print("number is zero")
+    
+    #output
+    """Enter the number: -7
+number is negative
+
+Enter the number: 0
+number is zero
+
+Enter the number: 5
+number is positive """

ccb32ad3d9b6a1f2a2b878a64070830c91b1b741
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 10:21:02 +0530

    Add files via upload

---
+++ problem1.py/02/26)/problem1.py
@@ -0,0 +1,11 @@
+# Read input from standard input
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+# Swap two numbers without using a third variable
+a, b = b, a
+
+# Print the required output
+print("After swapping:")
+print("a =", a)
+print("b =", b)

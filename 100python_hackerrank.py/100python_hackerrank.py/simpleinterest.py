5523a01e9f01c23981466a6fa6b6be61d8deaba8
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 10:55:49 +0530

    Add files via upload

---
+++ 100python_hackerrank.py/100python_hackerrank.py/simpleinterest.py
@@ -0,0 +1,17 @@
+#calculate simple interest
+
+P = float(input("enter the principal: "))
+R = float(input("enter the rate:" ))
+T = float(input("enter the time:"))
+
+# Calculate Simple Interest
+SI = (P * R * T) / 100
+
+# Print result
+print("the simple interest is : ", SI)
+
+#output
+"""enter the principal: 23
+enter the rate:20
+enter the time:4
+the simple interest is :  18.4"""

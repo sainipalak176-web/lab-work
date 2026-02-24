dc23c09e0fb640a0dc9f3741d92122a9a47ee73e
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 11:21:38 +0530

    Add files via upload

---
+++ 100python_hackerrank.py/100python_hackerrank.py/convertTtoF.py
@@ -0,0 +1,13 @@
+#Convert temperature from Celsius to Fahrenheit
+# Read input
+C = float(input("tenperature in celsius: "))
+
+# Convert to Fahrenheit
+F = (9/5) * C + 32
+
+# Print result
+print("Temperature in fahrenheit:" ,F)
+
+#output
+"""tenperature in celsius: 30
+Temperature in fahrenheit: 86.0"""

07290df3c166ae6ccce7919dfbe3b3c919a80c78
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 11:01:11 +0530

    Add files via upload

---
+++ 100python_hackerrank.py/100python_hackerrank.py/largestOfThree.py
@@ -0,0 +1,13 @@
+#find largest of three numbers
+a = int(input("Enter the value1: "))
+b = int(input("Enter the value2: "))
+c = int(input("Enter the value3: "))
+
+print("the highest value is: ",max(a,b,c))
+
+#output
+"""Enter the value1: 5
+Enter the value2: 7
+Enter the value3: 9
+the highest value is:  9
+ """

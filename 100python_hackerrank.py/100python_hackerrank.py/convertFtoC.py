b6106f26582a7117a52dadd94a76ac87b003ccc8
sainipalak176-web <sainipalak176@gmail.com>
2026-02-24 11:23:45 +0530

    Add files via upload

---
+++ 100python_hackerrank.py/100python_hackerrank.py/convertFtoC.py
@@ -0,0 +1,13 @@
+#: Convert temperature from Fahrenheit to Celsius.
+# Read input
+F = float(input("temperature in fahrenheit: "))
+
+# Convert to Celsius
+C = (5/9) * (F - 32)
+
+# Print result
+print("temperature in celsius:" ,C)
+
+#output
+"""temperature in fahrenheit: 100
+temperature in celsius: 37.77777777777778"""
